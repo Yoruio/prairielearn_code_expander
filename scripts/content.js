14c97e6f49e257dcff274378fc313245566e26cd
Yoruio <zeleidu@gmail.com>
2022-10-21 17:09:37 -0700

    removes background and outline from code block

--- scripts/content.js
+++ scripts/content.js
@@ -4,5 +4,12 @@ const content_div = document.querySelector('#content');
 if (file_preview) {
     file_preview.classList.add('show');
     file_preview.classList.remove('file-preview');
-    content_div.classList.remove('container');
+    if (content_div) {
+        content_div.classList.remove('container');
+    }
+    code = file_preview.querySelector('code');
+    if (code) {
+        code.style.background="none";
+        code.style.border="none";
+    }
 }
