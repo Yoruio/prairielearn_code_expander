fb5cb988a3738073623ce5b17e7cf99cb78fe37a
Yoruio <zeleidu@gmail.com>
2022-10-21 18:13:59 -0700

    do expansion for all files if multiple

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,28 @@
-const file_preview =document.querySelector('.file-preview');
+const file_preview_elements = document.getElementsByClassName('file-preview');
 const content_div = document.querySelector('#content');
 
-if (file_preview) {
+for (var i = 0; i < file_preview_elements.length; i++) {
+    var file_preview = file_preview_elements[i];
     file_preview.classList.add('show');
     file_preview.classList.remove('file-preview');
-    if (content_div) {
-        content_div.classList.remove('container');
-    }
-    code = file_preview.querySelector('code');
+    var code = file_preview.querySelector('code');
     if (code) {
         code.style.background="none";
         code.style.border="none";
     }
-    hljs.highlightAll();
 }
+
+file_preview.classList.add('show');
+file_preview.classList.remove('file-preview');
+
+if (content_div) {
+    content_div.classList.remove('container');
+}
+
+var code = file_preview.querySelector('code');
+if (code) {
+    code.style.background="none";
+    code.style.border="none";
+}
+
+hljs.highlightAll();
