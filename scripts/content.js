3583a63a601702ae028dbabd2a608fd77969a3e5
Yoruio <zeleidu@gmail.com>
2022-10-21 21:02:22 -0700

    fixes duplicate code bug

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,12 @@
-const file_preview_elements = document.getElementsByClassName('file-preview');
+// widen content area
 const content_div = document.querySelector('#content');
+
+if (content_div) {
+    content_div.style['max-width'] = "100vw";
+}
+
+// deal with file previews
+const file_preview_elements = document.getElementsByClassName('file-preview');
 
 for (var i = 0; i < file_preview_elements.length; i++) {
     var file_preview = file_preview_elements[i];
@@ -10,19 +17,6 @@ for (var i = 0; i < file_preview_elements.length; i++) {
         code.style.background="none";
         code.style.border="none";
     }
-}
-
-file_preview.classList.add('show');
-file_preview.classList.remove('file-preview');
-
-if (content_div) {
-    content_div.style['max-width'] = "100vw";
-}
-
-var code = file_preview.querySelector('code');
-if (code) {
-    code.style.background = "none";
-    code.style.border = "none";
 }
 
 hljs.highlightAll();
